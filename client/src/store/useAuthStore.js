21a6f268174a08ea33b5c1a9b83e60833a29d9f6
farehaaslam <farehaaslam57@gmail.com>
2025-06-17 00:59:51 +0530

    project completed

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -2,7 +2,7 @@ import {create} from "zustand"
 import { axiosInstance } from "../lib/axios"
 import toast from "react-hot-toast"
 import { io} from "socket.io-client"
-const BASE_URL="http://localhost:3000/"
+const BASE_URL=import.meta.env.MODE==="development"?"http://localhost:3000/api":"/api"
 export const userAuthStore=create((set,get)=>({
     authUser:null,
     isSigningup:false,
--- server/index.js
+++ server/index.js
@@ -8,7 +8,9 @@ import userRouter from "./routes/user.routes.js"
 import messageRouter from "./routes/message.routes.js"
 import { app,server } from "./lib/socket.js"
 
+import path from "path"
 const PORT = process.env.PORT || 3000
+const __dirname=path.resolve()
 app.use(cors({
     origin:'http://localhost:5173',
     credentials:true
@@ -19,4 +21,10 @@ app.use (cookieParser())
 
 app.use("/api/user",userRouter)
 app.use("/api/message",messageRouter) 
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../client/dist")))
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../client","dist","index.html"))
+    })
+}
 startServer(app,PORT)
